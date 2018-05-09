0a1cdec170ef515a81aae22eec42af3e2414ad94
Shengwen Cheng <shengwen1997.tw@gmail.com>
2018-05-09 19:24:49 +0800

    Finish dff pratice

--- d_flip_flop/dff.v
+++ d_flip_flop/dff.v
@@ -2,8 +2,12 @@ module dff(input in_d, input clk, output out_q);
 
 reg out_q;
 
+initial begin
+out_q = 0;
+end
+
 always @(posedge clk) begin
-	out_q <= in_d;
+	out_q = in_d;
 end
 
 endmodule
--- d_flip_flop/test_dff.v
+++ d_flip_flop/test_dff.v
@@ -1,4 +1,40 @@
-`timescale 1ms/10us
+`timescale 1ms/1us
 
 module test_dff();
+
+reg in_d, clk;
+wire out_q;
+
+dff dff1(in_d, clk, out_q);
+
+//generate clock signal
+always #1 begin
+	clk = ~clk;
+end
+
+initial begin
+	in_d = 0;
+	clk = 0;
+	#1;
+
+	$dumpfile("sim_out.vcd");
+	$dumpvars(0, test_dff);
+
+	in_d = 1;
+	#2;
+
+	in_d = 0;
+	#1;
+
+	in_d = 1;
+	#2;
+
+	in_d = 0;
+	#4
+
+	$finish();
+end
+
 endmodule
+
+
